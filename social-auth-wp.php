03f522f5768ee78640fd5dd32ba3cbea9dea6eae
Viruthagiri Thirumavalavan <giridharanbtech@gmail.com>
2012-12-03 17:58:39 +0530

    cURL Requirements check

--- social-auth-wp.php
+++ social-auth-wp.php
@@ -18,6 +18,9 @@ add_action('init','SocialAuth_WP');
 /* Runs when plugin is activated */
 register_activation_hook(__FILE__,'SocialAuth_WP_install');
 
+/* cURL Requirements check */
+add_action('admin_notices', 'SocialAuth_WP_req_check');
+
 /* Runs on plugin deactivation*/
 register_deactivation_hook( __FILE__, 'SocialAuth_WP_remove' );
 
@@ -92,4 +95,29 @@ function get_user_by_meta( $meta_key, $meta_value ) {
     global $wpdb;
     $sql = "SELECT user_id FROM $wpdb->usermeta WHERE meta_key = '%s' AND meta_value = '%s'";
     return $wpdb->get_var( $wpdb->prepare( $sql, $meta_key, $meta_value ) );
+}
+
+/* cURL Requirements check */
+function SocialAuth_WP_req_check() {
+if(!function_exists('curl_init')){
+echo '<div class="error"><p>';
+echo 'cURL library cannot be found. Make sure it is installed. Otherwise SocialAuth-WordPress will not work properly.';
+echo '</p></div>';
+exit;
+}
+$agent = "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4) Gecko/20030624 Netscape/7.1 (ax)";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/");
+curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+$returned=curl_exec ($ch);
+if($returned==null){
+echo '<div class="error"><p>';
+echo 'Your cURL does not allow https protocol. Make sure OpenSSL is installed. Otherwise SocialAuth-WordPress will not work properly.';
+echo '</p></div>';
+}
+curl_close ($ch);
 }
